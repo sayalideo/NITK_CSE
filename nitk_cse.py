c68d750bb9ff51e75886c3e4a775495ecda3d5f1
Sayali <sayalideo137@gmail.com>
2021-01-01 16:27:29 +0530

    Initial db created

--- nitk_cse.py
+++ nitk_cse.py
@@ -1,8 +1,30 @@
 from flask import Flask, render_template, flash, redirect, url_for
 from forms import LoginForm, NewsForm
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '9cda09e832928e461cdd567c2e2249c4'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(60), nullable=False)
+
+    def __repr__(self):
+        return f"User('{self.email}')"
+
+class News(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    news = db.Column(db.String(120), unique=True, nullable=False)
+    link = db.Column(db.String(120), unique=True, nullable=False)
+    link_text = db.Column(db.String(120), unique=True, nullable=False)
+
+    def __repr__(self):
+        return f"News('{self.news}')"
+
 @app.route("/")
 def hello():
     return 'hi'
